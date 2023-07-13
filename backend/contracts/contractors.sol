414dd1088c14b3ecfc3419278d619d4df595e6a0
FatGuyy <omkarjadhav2103@gmail.com>
2023-07-13 15:42:43 +0530

    contractor.sol add addWork function

--- backend/contracts/contractors.sol
+++ backend/contracts/contractors.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
+
+contract contractor{
+    // string task;
+    // mapping(string => uint256) public reqirementsToAmount;
+    string[] public works;
+    uint256[] public amounts;
+    uint256 totalPairsInMapping;
+
+    function addWork(string memory work, uint256 amount) public {
+        works.push(work);
+        amounts.push(amount);
+        totalPairsInMapping++;
+    }
+
+    // function requestToState() public returns(string memory, uint256){
+    // }
+}
