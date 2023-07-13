bb56976f6fa8145c01d11501b8b04623c1ef55da
FatGuyy <omkarjadhav2103@gmail.com>
2023-07-13 16:00:51 +0530

    add the Task in contracts.sol, basic in districts.sol

--- backend/contracts/District.sol
+++ backend/contracts/District.sol
@@ -2,3 +2,13 @@
 pragma solidity ^0.8.18;
 
 //This fill will also have the code for sectors 
+
+contract District{
+    address[] sectors;
+
+    function addSector(address newSector) public {
+        sectors.push(newSector);
+    }
+
+    // function 
+}
--- backend/contracts/contractors.sol
+++ backend/contracts/contractors.sol
@@ -1,18 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+struct Task{
+    string[] work;
+    uint256[] amount;
+}
 
 contract contractor{
-    // string task;
-    // mapping(string => uint256) public reqirementsToAmount;
     string[] public works;
     uint256[] public amounts;
-    // uint256 totalPairsInMapping;
+    Task task;
 
     function addWork(string memory work, uint256 amount) public {
         works.push(work);
         amounts.push(amount);
-        // totalPairsInMapping++;
+    }
+
+    function newTask() public{
+        task = Task(works,amounts);
     }
 
     // function requestToState() public returns(string memory, uint256){
